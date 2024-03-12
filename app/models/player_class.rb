5c9ee5dbf216450a2a61f0870cb8eb4810b66552
nfeng037 <nfeng@academic.rrc.ca>
2024-03-12 18:57:01 -0500

    Establish model association

--- app/models/player_class.rb
+++ app/models/player_class.rb
@@ -1,7 +1,7 @@
 class PlayerClass < ApplicationRecord
   self.primary_key = 'id'
 
-  has_many :heros
+  has_many :heroes
   has_many :specializations
   has_many :race_classes
   has_many :races, through: :race_classes
--- app/models/race.rb
+++ app/models/race.rb
@@ -1,7 +1,7 @@
 class Race < ApplicationRecord
   self.primary_key = 'id'
 
-  has_many :heros
+  has_many :heroes
   has_many :race_classes
   has_many :classes, through: :race_classes
 
